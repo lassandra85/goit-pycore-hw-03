4d9d5b57c0c08dfd84a8bb307eef4f29bbc94b5c
lassandra85 <lassandra85@gmail.com>
2024-10-20 01:27:05 +0200

    ht2

---
+++ ht2.py
@@ -0,0 +1,16 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+
+    correct_number = min < 1 or max > 1000 or quantity < 1 
+
+    if correct_number: 
+        return []
+    else:
+        random_numbers = random.sample(range(min, max + 1), quantity)
+    
+        return sorted(random_numbers)
+   
+
+
+print( "Ваші лотерейні числа:", get_numbers_ticket(1, 1000, 10))
